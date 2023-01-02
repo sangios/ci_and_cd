cf41ba4ad94b182c9d0f01c91a73f8219feb1311
Sang Nguyen <nguyensang1986@gmail.com>
2023-01-02 21:47:59 +0700

    Add swiftlint

---
+++ StudyCICD/StudyCICD/CodingConvetion.swift
@@ -0,0 +1,14 @@
+//
+//  CodingConvetion.swift
+//  StudyCICD
+//
+//  Created by user on 12/30/22.
+//
+
+import Foundation
+
+class CodingConvetion {
+    func testLineLength() {
+        let lineLengthShouldNotGreaterThan100Characters = "123456677777777777777777777777777777777777777"
+    }
+}
